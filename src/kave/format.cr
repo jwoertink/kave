2fc8cf99981490d490e6f46f69be596b5f5aef6b
Jeremy Woertink <jeremywoertink@gmail.com>
2016-11-28 10:12:32 -0800

    adding helper to convert objects to json properly

--- src/kave/format.cr
+++ src/kave/format.cr
@@ -5,5 +5,20 @@ module Kave
     def self.json
       {"content_type" => "application/json", "extension" => ".json"}
     end
+
+    def jsonize(obj)
+      case obj
+      when Array
+        obj.map {|o| jsonize(o)}.as(JSON::Type)
+      when Hash
+        h = {} of String => JSON::Type
+        obj.each {|k, v| h[k] = jsonize(v)}
+        h.as(JSON::Type)
+      when Int32
+        obj.to_i64.as(JSON::Type)
+      else
+        obj.as(JSON::Type)
+      end
+    end
   end
 end
