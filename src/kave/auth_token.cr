dd0c04353964ea3697ae3d6393c7fe1eff38bef3
Jeremy Woertink <jeremywoertink@gmail.com>
2017-03-04 18:50:25 -0800

    revert because you can't define an abstract method on a metaclass

--- src/kave/auth_token.cr
+++ src/kave/auth_token.cr
@@ -2,6 +2,8 @@ module Kave
   abstract class AuthToken
     
     # If "truthy" then the token has been found.
-    abstract def self.locate(token : String)
+    def self.locate(token : String)
+      true
+    end
   end
 end
