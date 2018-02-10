64aa48fda9291fa3fb1ae2a58fa4d47e4857661a
Jeremy Woertink <jeremywoertink@gmail.com>
2018-02-10 14:58:17 -0800

    renamed path_option to version_strategy. It's a little clearer, and will make more sense for other options

--- src/kave/config.cr
+++ src/kave/config.cr
@@ -1,19 +1,20 @@
 module Kave
   class Config
-    getter auth_strategy
+    getter auth_strategy, version_strategy
     property token_model, format, public_routes
 
     def initialize
       @auth_strategy = nil.as(Symbol?)
+      @version_strategy = :path
       @token_model = Kave::AuthToken
       @format = :json
-      @public_routes = [] of String
+      @public_routes = Hash(String, Array(String)).new([] of String)
     end
 
-    def auth_strategy=(new_strategy : Symbol)
-      strategy = case new_strategy
+    def auth_strategy=(option : Symbol)
+      strategy = case option
                  when :bearer
-                   add_handler Kave::BearerHandler.new
+                   add_handler(Kave::BearerHandler.new, 0)
                    :bearer
                  else
                    nil
@@ -25,10 +26,16 @@ module Kave
     #   curl http://localhost:3000/v1/test
     #       vs
     #   curl -H "Accept: application/vnd.api.v1+json" http://localhost:3000/test
-    def path_option=(option : String?)
-      if option == "header"
-        add_handler Kave::RouteHeaderHandler.new
-      end
+    def version_strategy=(option : Symbol)
+      strategy = case option
+                 when :header
+                   add_handler(Kave::HeaderVersionStrategyHandler.new, 0)
+                   :header
+                 else
+                   :path
+                 end
+
+      @version_strategy = strategy
     end
   end
 end
